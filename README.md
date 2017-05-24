5c9d5c42dd688d53cccdc14f123565ebb7364465
Jonathan Simpson <jsimpson.github@gmail.com>
2017-05-23 19:30:50 -0700

    initial

---
+++ README.md
@@ -0,0 +1,37 @@
+# GitHub Organization Reports
+
+This was adapted from a [gist](https://gist.github.com/jdennes/11404512) by user @[jdennes](https://github.com/jdennes).
+
+It is a very crude way to run a loose audit against your GitHub organizations users and discover their active/inactive state. I believe that this is a feature in GitHub Enterprise that, for whatever reason, to my knowledge, doesn't exist for a GitHub hosted Organization.
+
+## What does it do?
+
+It pulls down all of your orgainzations members and repositories. It then analyzes repository events and correlates them back to your users. It updates 2 pertinent fields: `active` state (`last_activity` >= `CUTOFF` in days) and `last_activity` of the organization repository event that user. It spits all of this out as a simple CSV.
+
+## Set up
+
+Clone the repository
+
+```bash
+$ git clone https://github.com/jsimpson/github-organization-report.git .; cd github-organization-report
+```
+
+Install Octokit
+
+```bash
+bundle install
+```
+
+Configure the script
+
+Set `ORGANIZAION` to your organization name.
+
+Set `CUTOFF` appropriately.
+
+## Execute
+
+```bash
+$ OCTOKIT_ACCESS_TOKEN=<yourtoken> bundle exec ruby organization-reports.rb
+$ cat report.csv
+```
+
---
+++ organization-reports.rb
@@ -0,0 +1,62 @@
+require "csv"
+require "octokit"
+
+# default to 90 days
+CUTOFF = DateTime.now - 90
+
+# your organization name
+ORGANIZATION = ""
+
+User = Struct.new(:username, :full_name, :active, :last_activity)
+Event = Struct.new(:username, :repository, :date)
+
+def events
+  @events ||= []
+end
+
+def users
+  @users ||= []
+end
+
+def find_user_by_username(username)
+  users.select { |user| user[:username] == username }.first
+end
+
+Octokit.auto_paginate = true
+members = Octokit.org_members ORGANIZATION
+repos = Octokit.org_repos ORGANIZATION
+
+members.each do |member|
+  user = Octokit.user member[:login]
+  users << User.new(member[:login], user[:name], false, nil)
+end
+
+repos.each do |repo|
+  repo_events = Octokit.repository_events "#{ORGANIZATION}/#{repo[:name]}"
+  repo_events.each { |event| events << Event.new(event[:actor][:login], repo[:name], event[:created_at]) }
+end
+
+events.each do |event|
+  user = find_user_by_username(event[:username])
+  next if user.nil?
+
+  if user[:last_activity].nil?
+    user[:last_activity] = event[:date]
+  else
+    user[:last_activity] = event[:date] if event[:date] >= user[:last_activity]
+  end
+end
+
+users.each do |user|
+  next if user[:last_activity].nil?
+
+  user[:active] = true if user[:last_activity].to_datetime >= CUTOFF
+end
+
+CSV.open("report.csv", "wb") do |csv|
+  csv << ["username", "full name", "active", "last_activity"]
+  users.each do |user|
+    csv << [user[:username], user[:full_name], user[:active], user[:last_activity]]
+  end
+end
+
